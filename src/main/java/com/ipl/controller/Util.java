80e1b42d5c698ce9b3347e394d9db46be5388525
nkg447 <nikunj.gupta447@gmail.com>
2019-04-17 12:36:39 +0530

    +(app level date string utility)

--- src/main/java/com/ipl/controller/Util.java
+++ src/main/java/com/ipl/controller/Util.java
@@ -10,11 +10,14 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Util {
 	public static final ObjectMapper MAPPER = new ObjectMapper();
 	public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 	final static Logger logger = Logger.getLogger(Util.class);
+	private static final int DAY_DIFFERENCE = 0;
 
 	public static String sendGet(String url) {
 		try {
@@ -42,6 +45,26 @@ public class Util {
 			logger.error(e.getMessage(), e);
 		}
 		return null;
+	}
+
+	public static String todayDateString() {
+		logger.debug("today date - " + getDateString(DAY_DIFFERENCE));
+		return getDateString(DAY_DIFFERENCE);
+	}
+
+	public static String yesturdayDateString() {
+		logger.debug("yesturday date - " + getDateString(-1 + DAY_DIFFERENCE));
+		return getDateString(-1 + DAY_DIFFERENCE);
+	}
+
+	public static String getDateString(int day_difference) {
+		return DATE_FORMAT.format(today(day_difference));
+	}
+
+	public static Date today(int day_difference) {
+		final Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, day_difference);
+		return cal.getTime();
 	}
 
 	public static String toStringOf(Object o) {
