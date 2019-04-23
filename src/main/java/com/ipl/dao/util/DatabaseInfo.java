fb642d647da42ae923b9f2f9d733a95c2bcf2fae
nkg447 <nikunj.gupta447@gmail.com>
2019-04-23 18:21:26 +0530

    database name to lowercase

--- src/main/java/com/ipl/dao/util/DatabaseInfo.java
+++ src/main/java/com/ipl/dao/util/DatabaseInfo.java
@@ -1,10 +1,10 @@
 package com.ipl.dao.util;
 
 public class DatabaseInfo {
-	public static final String PREDICTION = "PREDICTION";
-	public static final String PREDICTOR = "PREDICTOR";
-	public static final String QUESTION = "QUESTION";
-	public static final String ANSWER = "ANSWER";
-	public static final String AUTHENTICATION = "AUTHENTICATION";
+	public static final String PREDICTION = "PREDICTION".toLowerCase();
+	public static final String PREDICTOR = "PREDICTOR".toLowerCase();
+	public static final String QUESTION = "QUESTION".toLowerCase();
+	public static final String ANSWER = "ANSWER".toLowerCase();
+	public static final String AUTHENTICATION = "AUTHENTICATION".toLowerCase();
 
 }
