67758b9e82aedc936c859f8e3cbb6bbfdbc67687
nkg447 <nikunj.gupta447@gmail.com>
2019-04-24 17:50:23 +0530

    Revert "register form extends login form"
    This reverts commit ad2a73feddf2362def3f3e05b8a0914634d70015.

--- src/main/java/com/ipl/form/RegisterForm.java
+++ src/main/java/com/ipl/form/RegisterForm.java
@@ -3,21 +3,25 @@ package com.ipl.form;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-public class RegisterForm extends LoginForm {
+public class RegisterForm extends Form {
 	private String name;
+	private String email;
+	private String password;
 
 	public RegisterForm() {
 	}
 
 	public RegisterForm(String name, String email, String password) {
-		super(email, password);
 		this.name = name;
+		this.email = email;
+		this.password = password;
 	}
 
 	@Override
 	public void validate() throws ValidationException {
-		super.validate();
+		FormUtil.validateEmail(getEmail());
 		FormUtil.validateName(getName());
+		FormUtil.validatePassword(getPassword());
 	}
 
 	@Override
@@ -35,5 +39,21 @@ public class RegisterForm extends LoginForm {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
